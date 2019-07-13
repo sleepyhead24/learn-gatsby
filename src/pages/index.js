b350bb77cfb5f25e4299a049b8fd99ec796b161a
Kenny Kang <kennykcy@Kennys-MacBook-Pro.local>
2019-07-13 11:30:15 +0800

    add not found page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>Kenny Kang.</p>
+        </div>
+    );
+};
+
+export default HomePage;
+
